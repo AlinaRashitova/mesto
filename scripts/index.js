bac9a5cd62f041b2ccced2fa0b67634900775592
Alina Rashitova <alinarashitova02@yandex.ru>
2022-08-10 14:43:51 +0500

    Fourth project

--- scripts/index.js
+++ scripts/index.js
@@ -1,10 +1,31 @@
 const popup = document.querySelector('.popup');
-const openButton = document.querySelector('.profile__edit-button_popup-open');
+const editButton = document.querySelector('.profile__edit-button');
 const closeButton = popup.querySelector('.popup__close-icon');
+const popupContainer = popup.querySelector('.popup__container');
+
+let popupForm = popup.querySelector('.popup__form');
+let nameInput = popupForm.querySelector('popup__name');
+let jobInput = popupForm.querySelector('popup__job');
+let profileName = document.querySelector('.profile__title');
+let profileJob = document.querySelector('.profile__subtitle');
 
 const togglePopup = function () {
   popup.classList.toggle('popup__opened');
 }
 
-openButton.addEventListener('click', togglePopup)
+popup.addEventListener('click', function(event) {
+  if (event.target === event.currentTarget) {
+    togglePopup();
+  }
+})
+
+function formSubmitHandler (evt) {
+    evt.preventDefault();
+    profileName.textContent = nameInput.value;
+    profileJob.textContent = jobInput.value;
+    togglePopup();
+}
+
+editButton.addEventListener('click', togglePopup)
 closeButton.addEventListener('click', togglePopup)
+popupForm.addEventListener('submit', formSubmitHandler);
