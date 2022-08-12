3438cc2100a6645b3f31ec45d32a5f368da955d8
Alina Rashitova <alinarashitova02@yandex.ru>
2022-08-12 12:29:55 +0500

    Fourth project

--- scripts/index.js
+++ scripts/index.js
@@ -1,30 +1,35 @@
 let popup = document.querySelector('.popup');
 let editButton = document.querySelector('.profile__edit-button');
-let closeButton = popup.querySelector('.popup__close-icon');
+let closeButton = popup.querySelector('.popup__close-button');
 let popupForm = popup.querySelector('.popup__form');
-let nameInput = popupForm.querySelector('.popup__name');
-let jobInput = popupForm.querySelector('.popup__job');
+let nameInput = popupForm.querySelector('#nameInput');
+let jobInput = popupForm.querySelector('#jobInput');
 let saveButton = popup.querySelector('.popup__save-button');
 let profileName = document.querySelector('.profile__title');
 let profileJob = document.querySelector('.profile__subtitle');
 
-let namePopup = function () {
+function popupFormValue() {
   nameInput.setAttribute('value', profileName.textContent);
   jobInput.setAttribute('value', profileJob.textContent);
 }
 
-let togglePopup = function () {
-  popup.classList.toggle('popup__opened');
-  namePopup();
+function openPopup() {
+  popupFormValue();
+  popup.classList.add('popup_opened');
 }
+
+function closePopup() {
+  popup.classList.remove('popup_opened');
+}
+
+editButton.addEventListener('click', openPopup);
+closeButton.addEventListener('click', closePopup);
+saveButton.addEventListener('click', closePopup);
 
 function formSubmitHandler(evt) {
   evt.preventDefault();
   profileName.textContent = nameInput.value;
   profileJob.textContent = jobInput.value;
-  togglePopup();
 }
 
-editButton.addEventListener('click', togglePopup);
-closeButton.addEventListener('click', togglePopup);
 popupForm.addEventListener('submit', formSubmitHandler);
